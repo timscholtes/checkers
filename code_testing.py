b5c4f93ea1648ff5fab235e5d61e668dd2671584
Tim Scholtes <tim.scholtes@datasciencenetwork.com>
2016-09-13 17:19:55 +0100

    now full working play loop and also neural net

--- code_testing.py
+++ code_testing.py
@@ -1,35 +1,5 @@
 
-def intersperse(seq, value):
-    res = [value] * (2 * len(seq) - 1)
-    res[::2] = seq
-    return res
 
+KINGS_ROW  = {1: list(range(29, 33)),2: list(range(1, 5))}
 
-def board_list_viz():
-	
-	mid_row_even = [".","_"]*4
-	mid_row_odd  = ["_","."]*4
-	blank_board  = [
-	mid_row_even,mid_row_odd,
-	mid_row_even,mid_row_odd,
-	mid_row_even,mid_row_odd,
-	mid_row_even,mid_row_odd,
-	mid_row_even,mid_row_odd,
-	mid_row_even,mid_row_odd,
-	mid_row_even,mid_row_odd,
-	mid_row_even,mid_row_odd
-	]
-	
-	return blank_board
-
-#print(board_list_viz())
-
-start_board = [val for sublist in [["1"]*12,["0"]*8,["-1"]*12] for val in sublist]
-
-start_board2 = intersperse(start_board,".")
-
-n = 8
-
-
-start_board2 = [start_board2[i:i + n] for i in range(0, len(start_board2), n)]
-print(start_board2)
+print(KINGS_ROW)
