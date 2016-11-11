aa478eeebb5dc2d71bdde43bb8018925e6056896
Tim Scholtes <tim.scholtes@datasciencenetwork.com>
2016-11-11 11:12:28 +0000

    included downloaded data from google storage into gsp_results folder, code_testing.py now includes a test match

--- py2_7/code_testing.py
+++ py2_7/code_testing.py
@@ -4,13 +4,24 @@ from nn_methods import *
 player1=alphabeta_player
 player2=alphabeta_player
 
+W1 = np.load('gcp_results/checkers-144720/data_4/top_player/W1.npy')
+W2 = np.load('gcp_results/checkers-144720/data_4/top_player/W2.npy')
+W3 = np.load('gcp_results/checkers-144720/data_4/top_player/W3.npy')
+W4 = np.load('gcp_results/checkers-144720/data_4/top_player/W4.npy')
+nnet_1 = {'W1': W1,'W2': W2,'W3': W3, 'W4': W4}
 
-nnets = {1: generate_player_nn(),-1: generate_player_nn()}
+W1 = np.load('gcp_results/checkers-144720/data/top_player/W1.npy')
+W2 = np.load('gcp_results/checkers-144720/data/top_player/W2.npy')
+W3 = np.load('gcp_results/checkers-144720/data/top_player/W3.npy')
+W4 = np.load('gcp_results/checkers-144720/data/top_player/W4.npy')
+nnet_2 = {'W1': W1,'W2': W2,'W3': W3, 'W4': W4}
+
+
+print(nnet_2['W4'])
+nnets = {1: nnet_2,-1: nnet_1}
 
 checkers=checkers_class()
-result1 = play_game(checkers,nnets,True,2,player1,player2)
 
-W1 = np.load('data/top_player/W1.npy')
-W2 = np.load('data/top_player/W2.npy')
-W3 = np.load('data/top_player/W3.npy')
-W4 = np.load('data/top_player/W4.npy')
+
+#result1 = play_game(checkers,nnets,True,4,player1,player2)
+
